50cd843ddf91916185f650f65e68f32eb0c0e20e
WhoisJohnKim <johnkim9417@gmail.com>
2018-09-06 23:40:43 -0500

    Finished the first draft of the burger model and also added in the main.handlebars

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(result) {
+            cb(result);
+        });
+    },
+    // cols and vals are arrays
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(results) {
+            cb(results);
+        });
+    },
+    updateOne: function(objColVals, condition cb) {
+        orm.updateOne("burgers", objColVals, condition, function(results) {
+            cb(results);
+        });
+    }
+};
+
+module.exports = burger;
