f1e42ffe7b92a2d1ea86cdc1d3982b4451b3bd20
WhoisJohnKim <johnkim9417@gmail.com>
2018-09-06 22:56:24 -0500

    Finished connection.js and the schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES (`Kamikaze Burger`);
+INSERT INTO burgers (name) VALUES (`Whopper`);
+INSERT INTO burgers (name, devoured) VALUES (`Big Mac`, true);
+INSERT INTO burgers (name, devoured) VALUES (`Portobello`, false);
