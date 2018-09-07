2c0ad1fd9bbc2bb5cac2c2b6be48babf2d26115e
WhoisJohnKim <johnkim9417@gmail.com>
2018-09-06 23:00:04 -0500

    Fixed seeds.sql. Had name instead burger_name and now using '' instead of

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (name) VALUES (`Kamikaze Burger`);
-INSERT INTO burgers (name) VALUES (`Whopper`);
-INSERT INTO burgers (name, devoured) VALUES (`Big Mac`, true);
-INSERT INTO burgers (name, devoured) VALUES (`Portobello`, false);
+INSERT INTO burgers (burger_name) VALUES ('Kamikaze Burger');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello', false);
