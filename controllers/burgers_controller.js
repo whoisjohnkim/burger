b11cfd82961f9c366ba9ea570f3a0f05ba3bcee0
WhoisJohnKim <johnkim9417@gmail.com>
2018-09-07 22:59:04 -0500

    Finished server.js and set up draft of burgers_controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,36 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function(data){
+        var burgerObject = {
+            burgers: data
+        };
+        console.log(burgerObject);
+        res.render("index", burgerObject)
+    });
+});
+
+router.post("/api/burgers", function(req, res){
+    burger.create("burger_name", req.body.burger_name, function(result) {
+        res.json({ id: result.insertId })
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res){
+    var condition = `id = ${req.params.id}`
+    console.log(`condition is ${condition}`)
+
+    burger.update(condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+})
+
+module.exports = router;
